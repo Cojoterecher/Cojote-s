300047a1ee094eaeaa9388f9dfa35f6056d36aed
Cojoterecher <87228080+Cojoterecher@users.noreply.github.com>
2026-06-12 01:54:16 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "on Cojote's Network",
+        name: "Welcome on Cojote's Network!",
         // Activity type number (0 = Playing).
         type: 3, 
       },
